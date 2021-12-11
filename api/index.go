208db110c0bba76665fbab50009a8c8c7fc0d2d0
秋のかえで <autmaple@protonmail.com>
2021-12-11 22:17:27 +0800

    feat(vercel): refine handler

--- api/index.go
+++ api/index.go
@@ -14,23 +14,28 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var ctx = context.Background()
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	b := session.BotFromContext(ctx)
+
+	var u tb.Update
+
+	body, err := io.ReadAll(r.Body)
+	common.Must(err)
+
+	common.Must(json.Unmarshal(body, &u))
+
+	b.ProcessUpdate(u)
+}
+
+func init() {
 	b, err := tb.NewBot(tb.Settings{
 		Token:       os.Getenv("KIRIRIN_TELEGRAM_TOKEN"),
 		Synchronous: true,
 	})
 	common.Must(err)
 
-	ctx := session.ContextWithBot(context.Background(), b)
-
+	ctx = session.ContextWithBot(ctx, b)
 	features.Handle(ctx)
-
-	var u tb.Update
-
-	body, err := io.ReadAll(r.Body)
-
-	common.Must(err)
-	common.Must(json.Unmarshal(body, &u))
-
-	b.ProcessUpdate(u)
 }
